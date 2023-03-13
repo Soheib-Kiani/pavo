06f17f4bb411fb1927299719565e17f104ab8644
soheib kiani <mrsoheibkiani@gmail.com>
2023-03-13 23:29:32 +0330

    Add Features Compoent

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Header from '../components/header';
 import Main from '../components/main';
+import Features from '../components/features';
 export default function Home() {
   return (
     <>
       <Header />
       <Main />
+      <Features />
     </>
   );
 }
