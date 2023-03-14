396746259d782f668a7d25568ae24df4f173b322
soheib kiani <mrsoheibkiani@gmail.com>
2023-03-14 19:10:51 +0330

    Edit Numbers In Price Components

--- components/statistics.js
+++ components/statistics.js
@@ -4,32 +4,32 @@ const statistics = () => {
       <div className="container px-4 sm:px-8">
         <div id="counter">
           <div className="cell">
-            <div className="counter-value number-count" data-count="231">
-              1
+            <div className="counter-value number-count">
+              65
             </div>
             <p className="counter-info">Happy Users</p>
           </div>
           <div className="cell">
-            <div className="counter-value number-count" data-count="385">
-              1
+            <div className="counter-value number-count">
+              45
             </div>
             <p className="counter-info">Issues Solved</p>
           </div>
           <div className="cell">
-            <div className="counter-value number-count" data-count="159">
-              1
+            <div className="counter-value number-count">
+              12
             </div>
             <p className="counter-info">Good Reviews</p>
           </div>
           <div className="cell">
-            <div className="counter-value number-count" data-count="127">
-              1
+            <div className="counter-value number-count">
+              173
             </div>
             <p className="counter-info">Case Studies</p>
           </div>
           <div className="cell">
-            <div className="counter-value number-count" data-count="211">
-              1
+            <div className="counter-value number-count">
+              45
             </div>
             <p className="counter-info">Orders Received</p>
           </div>
